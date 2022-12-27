6a8646f80cddd2b0c20380a5581c0d1ad503591f
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2022-12-27 20:06:05 +0530

    ok

--- java assigment/14.java
+++ java assigment/14.java
@@ -1,10 +1,12 @@
 //no divisible by 2,3 and 5
+// me using if condition and nested if
 class ok{
 	public static void main(String args[]){
 		for (int i=1;i<=150;i++){
 			if (i%2==0){
 				if(i%3==0&&i%5==0){
 					System.out.println(i);
+					
 				}
 			}
 			
