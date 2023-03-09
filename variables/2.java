137926ec3a3d144640eed99fa3718a5ef3452888
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2023-03-09 18:39:10 +0530

    var

---
+++ variables/2.java
@@ -0,0 +1,17 @@
+//variables
+//local variable inside main method  //static outside main method using ststic key word
+//instance varibale out side main method but do not have key word
+class add{
+	public static void main(String args[]){
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(c);
+		add e=new add();
+		System.out.println(e.d);
+      
+	}
+	static String c="chirag";
+	
+	int d=33;		
+	}
